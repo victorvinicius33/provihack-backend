8917fbb2c6364be85b579aa88d472311332f6b15
victor vinicius <victorvinicius33.vv@gmail.com>
2022-04-30 03:36:44 -0300

    configuração da conexão com o database na heroku

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'provihack',
+        host: 'ec2-34-194-73-236.compute-1.amazonaws.com',
+        user: 'oqjppcekekeags',
+        password: '32c65aeae229c12d5a42a85ed438e3b65bad79c63979d3c8f04b12943e421b2a',
+        database: 'datbjt5qecimqt',
+        port: 5432,
         ssl: {
             rejectUnauthorized: false
         }
